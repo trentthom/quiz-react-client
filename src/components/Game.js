d3da5dd9715c28bec332b9b0a4561cb4cd04aed9
trentthom <trent.tt@gmail.com>
2021-03-08 15:47:44 +1100

    question

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,21 +2,49 @@ import React, { Component } from "react"
 import data from './dummydata'
 import axios from 'axios'
 
-//onst SERVER_URL = //fill this out
+const TOPIC_URL = 'http://localhost:3000/topics'
+const QUESTION_URL = 'http://localhost:3000/questions'
+const ANSWERS_URL = 'http://localhost:3000/answers'
+
 
 class Game extends React.Component {
   constructor(props) {
     super()
+      this.state = {
+        question: '',
+        answers1: '',
+        ans2: '',
+      }
   }
 
+  // componentDidMount(){
+  //   axios.get(TOPIC_URL).then((response) => {
+  //     console.log(response)
+  //     //this.setState({sounds: padData})
+  //   })
+  // }
+
+  componentDidMount(){
+    axios.get(QUESTION_URL).then((response) => {
+      console.log(response.data[0])
+      const question = response.data[0].content
+      this.setState({question: question})
+
+      axios.get(ANSWERS_URL).then((response) => {
+        console.log(response.data)
+        const question = response.data[0].content
+        // this.setState({question: question})
+      })
+    })
+  }
 
   render() {
     return(
       <div>
-        <div className='question'><h1>Question</h1></div>
+        <div className='question'><h1>{this.state.question}</h1></div>
 
         <div style={{backgroundColor: 'red'}}className="container">
-          <div style={{lineHeight: '100px', backgroundColor: 'blue'}}>multi choise answer</div>
+          <div style={{lineHeight: '100px', backgroundColor: 'blue'}}></div>
           <div style={{lineHeight: '100px'}}>multi choise answer</div>
           <div style={{lineHeight: '100px'}}>multi choise answer</div>
           <div style={{lineHeight: '100px'}}>multi choise answer</div>
